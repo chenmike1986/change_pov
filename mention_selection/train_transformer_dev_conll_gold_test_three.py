97cd4c973989cdaee4724d85e0feb3efda763222
chenmike1986 <73728731+chenmike1986@users.noreply.github.com>
2021-08-10 12:56:02 +0800

    Update and rename train_transformer_dev_conll_gold_test_three_v2.py to train_transformer_dev_conll_gold_test_three.py

--- mention_selection/train_transformer_dev_conll_gold_test_three.py
+++ mention_selection/train_transformer_dev_conll_gold_test_three.py
@@ -3102,7 +3102,7 @@ def main():
 
     parser.add_argument('--conll_data_dir',
                     type=str,
-                    default='E:/mention_predictor_upload/code/preprocess/generate_training_data/output_training_array/conll_padding',
+                    default='change_pov/preprocessing/generate_training_data/output_training_array/conll_padding',
                     help='Directory to the conll training data.')
     parser.add_argument('--development_data_dir',
                     type=str,
